ab6381c766fb1aeeda7d1b77a9b0b4ce7b54130d
HuaJiKing402 <huajiking402@outlook.com>
2023-12-02 14:50:35 +0800

    first

---
+++ Models/UserItem.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GenesisSkin.Models
+{
+    [Table("user")]
+    public class UserItem
+    {
+        /// <summary> 用户ID </summary>
+        public long Id { get; set; }
+
+        /// <summary> 邮箱 </summary>
+        public required string Email { get; set; }
+
+        /// <summary> 昵称 </summary>
+        public required string Nickname { get; set; }
+
+        /// <summary> 密码 </summary>
+        public required string PasswordHash { get; set; }
+
+        public long Score { get; set; }
+
+        /// <summary> 盐 </summary>
+        public required string Salt { get; set; }
+    }
+}
---
+++ Models/UserItemDTO.cs
@@ -0,0 +1,16 @@
+﻿namespace GenesisSkin.Models
+{
+    public class UserItemDTO
+    {
+        /// <summary> 用户ID </summary>
+        public long Id { get; set; }
+
+        /// <summary> 邮箱 </summary>
+        public required string Email { get; set; }
+
+        /// <summary> 昵称 </summary>
+        public required string Nickname { get; set; }
+
+        public long Score { get; set; }
+    }
+}
